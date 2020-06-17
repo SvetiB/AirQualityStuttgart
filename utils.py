dfb386cb0ea4e7a034f3ea23359f43bf288e5a5f
Svetislav Bratic <bratic@gmx.de>
2020-06-17 20:31:14 +0200

    utils

---
+++ utils.py
@@ -0,0 +1,22 @@
+import pandas as pd  # u.a. verwendet um DataFrames zu generieren und zu bearbeiten
+import numpy as np
+import json  # Bearbeitung von json-Files
+import seaborn as sns # Visualisierungen
+from matplotlib import pyplot as plt
+
+
+def heatMap(df):
+    #Create Correlation df
+    corr = df.corr()
+    #Plot figsize
+    fig, ax = plt.subplots(figsize=(10, 10))
+    #Generate Color Map
+    colormap = sns.diverging_palette(220, 10, as_cmap=True)
+    #Generate Heat Map, allow annotations and place floats in map
+    sns.heatmap(corr, cmap=colormap, annot=False, fmt=".2f")
+    #Apply xticks
+    plt.xticks(range(len(corr.columns)), corr.columns);
+    #Apply yticks
+    plt.yticks(range(len(corr.columns)), corr.columns)
+    #show plot
+    plt.show()
