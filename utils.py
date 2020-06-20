1018664684958cf688ef22d3946c0deb7f713d66
Svetislav Bratic <bratic@gmx.de>
2020-06-20 21:36:25 +0200

    EDA, Missing value handling

--- utils.py
+++ utils.py
@@ -6,17 +6,22 @@ from matplotlib import pyplot as plt
 
 
 def heatMap(df):
-    #Create Correlation df
-    corr = df.corr()
-    #Plot figsize
-    fig, ax = plt.subplots(figsize=(10, 10))
-    #Generate Color Map
-    colormap = sns.diverging_palette(220, 10, as_cmap=True)
-    #Generate Heat Map, allow annotations and place floats in map
-    sns.heatmap(corr, cmap=colormap, annot=False, fmt=".2f")
-    #Apply xticks
-    plt.xticks(range(len(corr.columns)), corr.columns);
-    #Apply yticks
-    plt.yticks(range(len(corr.columns)), corr.columns)
-    #show plot
+    corr = df.corr() #Create Correlation df
+    fig, ax = plt.subplots(figsize=(10, 10)) #Plot figsize
+    colormap = sns.diverging_palette(220, 10, as_cmap=True) #Generate Color Map
+    sns.heatmap(corr, cmap=colormap, annot=False, fmt=".2f") #Generate Heat Map, allow annotations and place floats in map
     plt.show()
+
+def heatMapSmall(df):
+    corr = df.corr() #Create Correlation df
+    fig, ax = plt.subplots(figsize=(7, 7)) #Plot figsize
+    colormap = sns.diverging_palette(220, 10, as_cmap=True) #Generate Color Map
+    sns.heatmap(corr, cmap=colormap, annot=False, fmt=".2f") #Generate Heat Map
+    plt.show() #show plot
+
+def heatMapBig(df):
+    corr = df.corr() #Create Correlation df
+    fig, ax = plt.subplots(figsize=(12, 12)) #Plot figsize
+    colormap = sns.diverging_palette(220, 10, as_cmap=True) #Generate Color Map
+    sns.heatmap(corr, cmap=colormap, annot=False, fmt=".2f") #Generate Heat Map
+    plt.show() #show plot
